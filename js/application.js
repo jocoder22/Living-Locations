f62c8d11d78458555a2dbbd821813ae13bf19d15
Okigbo <okigbookey@gmail.com>
2017-12-14 14:07:23 -0500

    refact: Responsive attributes  On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   css/styles.css 	modified:   index.html 	modified:   js/application.js

--- js/application.js
+++ js/application.js
@@ -2,6 +2,7 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 40.733679, lng: -74.170804},
-    zoom: 14
+    zoom: 10
     });
+
 }
