e56dd612aadefb45b8fa87c07c8555985d00ef6b
Okigbo <okigbookey@gmail.com>
2017-12-15 10:14:14 -0500

    refactor: Add knockout model and Viewmodel  On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   js/application.js
    added model data and Viewmodel handlers

--- js/application.js
+++ js/application.js
@@ -1,8 +1,45 @@
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.733679, lng: -74.170804},
-    zoom: 10
+
+var myApp = function(){
+
+  var model = [
+    {name: 'Mummy Pot', address:'44 white park road newalk NJ 071122'},
+    {name: 'Cityplay22', address:'44 white park road newalk NJ 071122'},
+    {name: 'TigerMart', address:'44 white park road newalk NJ 071122'},
+    {name: 'Oceonview', address:'44 white park road newalk NJ 071122'},
+    {name: 'Boypharm', address:'44 white park road newalk NJ 071122'},
+    {name: 'Bestdeal', address:'44 white park road newalk NJ 071122'},
+    {name: 'Shopbig', address:'44 white park road newalk NJ 071122'},
+    {name: 'Mesuem90', address:'44 white park road newalk NJ 071122'}
+  ]
+
+
+  var map;
+  var myPlaces =  ko.obervableArray([]);
+
+  var updateMyPlaces = function () {
+    model.forEach(function(modelitem){
+      myPlaces.push(modelitem);
+      console.log(myPlaces());
+    });
+  };
+
+
+  showMap = function () {
+    map = new google.maps.Map({
+      center: {lat: 40.733679, lng: -74.170804},
+      zoom: 10
     });
 
-}
+  };
+
+  init = function () {
+    ko.applyBinding(myApp);
+  };
+
+  $(init);
+
+  return{
+    map: map,
+    myPlaces: myPlaces
+  };
+}();
