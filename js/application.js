a995b7e955fa9cea953b9495bae92e85780a052f
Okechukwu J. Okigbo <okigbookey@gmail.com>
2017-12-24 22:18:26 -0500

    feat: Add Ajax call to foursquare API
     Changes to be committed:
    	 modified:   js/application.js

--- js/application.js
+++ js/application.js
@@ -1,33 +1,58 @@
 
 var model = [
   {name: 'Robert Treat Center', address: '50 Park Pl,', city: 'Newark, NJ 07102', localLocation:[{lat: 40.739037 , lng: -74.168635}]},
-  {name: 'Newark Museum', address: '49 Washington St,', city: 'Newark, NJ 07102', localLocation:[{lat: 40.743108 , lng: -74.171716 }]},
+  {name: 'Newark Museum', address: '49 Washington St,', city: 'Newark, NJ 07102', localLocation:[{lat: 40.743108 , lng: -74.171716}]},
   {name: 'University Hospital', address: '150 Bergen St,', city:'Newark, NJ 07103', localLocation:[{lat: 40.740356 , lng: -74.190241 }]},
   {name: 'Newark ShopRite', address: '206 Springfield Ave', city:'Newark, NJ 07103', localLocation:[{lat: 40.736670 , lng: -74.186159 }]},
   {name: 'CityPlex 12', address: '360-394 Springfield Ave,', city: 'Newark, NJ 07103', localLocation:[{lat: 40.733553 , lng: -74.196379 }]},
   {name: "Applebee Grill", address: '383 Springfield Ave,', city: 'Newark, NJ 07103', localLocation:[{lat: 40.732625 , lng: -74.196325 }]},
   {name: "Wendy's Place", address: '427 Springfield Ave,', city: 'Newark, NJ 07103', localLocation:[{lat: 40.732158 , lng: -74.199454 }]},
-  {name: 'Home Depot', address: '399-443 Springfield Ave,', city: 'Newark, NJ 07103', localLocation:[{lat: 40.731358 , lng: -74.198435 }] }];
+  {name: 'Home Depot', address: '399-443 Springfield Ave,', city: 'Newark, NJ 07103', localLocation:[{lat: 40.731358 , lng: -74.198435 }]}];
+
+
+var myAddress = ['50 Park Pl, Newark NJ 07102', '49 Washington St, Newark NJ 07102', '150 Bergen St, Newark NJ 07103', '25 Lafayette St, Newark, NJ 07102', '360-394 Springfield Ave, Newark NJ 07103', '383 Springfield Ave, Newark NJ 07103', '94 William St, Newark, NJ 07102','399-443 Springfield Ave, Newark NJ 07103'];
 
 
 var viewModel = function(){
   var map;
   var markers = [];
-  var myPlaces = ko.observableArray();
+  var myPlaces = ko.observableArray([]);
 
 
-  var updateArray = function () {
-    model.forEach(function(PlaceItem){
-      myPlaces.push(PlaceItem);
-    });
-  };
+  $(function(){
+    for (var i = 0; i < myAddress.length; i++) {
+      var foursquareUrl = 'https://api.foursquare.com/v2/venues/search?limit=1&near=' + myAddress[i] + '&client_id=RMJLP0XC1CPMPOE4IINGN4RSBFUVTP10D1N0OO0RLBCCNPFK&client_secret=2OAXA5CSE43HQRNUYEEFV2FYR3SLB3CHVN0FCLXEGIUVCUUP&v=20170801'
+
+
+      $.getJSON(foursquareUrl, function(data){
+        format: 'json'
+      }).done(function(data){
+        var listings = {};
+        var resp = data.response.venues[0]; var llresp = resp.location;
+        listings.name = resp.name;
+        listings.stats = resp.stats;
+        listings.lat = llresp.lat;
+        listings.lng = llresp.lng;
+        listings.address = llresp.formattedAddress;
+        listings.localLocation = {};
+        listings.latlng = llresp.lat + ',' + llresp.lng;
+        listings.localLocation.lat = llresp.lat;
+        listings.localLocation.lng = llresp.lng;
+        myPlaces.push(listings);
+
+      }).fail(function(xhr, errorType, exception) {
+        alert( xhr.status + " " + errorType+"\n\ " + exception);
+      });
+    }
+
+  });
 
 
   var configureBindingHandlers = function() {
 		ko.bindingHandlers.mapper = {
 				init: function(element, valueAccessor){
 					map = new google.maps.Map(element, {
-            center: myPlaces()[4].localLocation[0],
+            center: model[3].localLocation[0],
 						zoom: 12
 					});
 
@@ -35,10 +60,10 @@ var viewModel = function(){
           var infowindowcontainer = new google.maps.InfoWindow();
 
           for (var i = 0; i < model.length; i++) {
-            var maklocation = myPlaces()[i].localLocation[0];
-            var makaddress = myPlaces()[i].address;
-            var maktitle = myPlaces()[i].name;
-            var makcity = myPlaces()[i].city;
+            var maklocation = model[i].localLocation[0];
+            var makaddress = model[i].address;
+            var maktitle = model[i].name;
+            var makcity = model[i].city;
             var marker = new google.maps.Marker({
               position: maklocation,
               map: map,
@@ -47,15 +72,12 @@ var viewModel = function(){
               cursor: '<h4>' + maktitle + '</h4>' + makaddress + '<br>' + makcity,
               id: i
             });
-            markers.push(marker);
-            console.log(marker.name);
 
+            markers.push(marker);
 
             this.showMarker = function(){
               populateInfoWindow(this, infowindowcontainer);
             }
-
-
 
             marker.addListener('mouseover', function(){
               populateInfoWindow(this, infowindowcontainer);
@@ -89,7 +111,7 @@ var viewModel = function(){
 
   init = function () {
     configureBindingHandlers();
-    updateArray();
+    //updateArray();
     ko.applyBindings(viewModel);
   };
 
